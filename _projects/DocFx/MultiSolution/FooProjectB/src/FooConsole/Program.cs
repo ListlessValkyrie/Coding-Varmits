6852e9bda0824d995685946b94c7585c69c9b0ac
NPC <NPC@ListlessValkyrie.com>
2021-07-02 11:27:18 +0100

    Initial commit
    Basic folder structure with some project templates.

---
+++ _projects/DocFx/MultiSolution/FooProjectB/src/FooConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FooConsole
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What a terrible application.".ToTimeStampedString());
+        }
+    }
+}
---
+++ _projects/DocFx/MultiSolution/Readme.md
@@ -0,0 +1,7 @@
+# Readme
+
+## DocFx setup
+
+See the [getting started guide](https://dotnet.github.io/docfx/tutorial/docfx_getting_started.html).
+
+* Created a sampled project: ```doxfx init -q``` 

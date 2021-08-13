d4eed0cc88823f45c3e8f5e0609bc11ab0901940
NPC <NPC@ListlessValkyrie.com>
2021-08-13 17:22:18 +0100

    Quick demo of using gridfs

---
+++ _projects/GridFS Demo/DB.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DB.Core;
+
+namespace DB.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileService fileService = new FileService(@"mongodb://localhost:27017");
+
+            string dbFileName = Guid.NewGuid().ToString();
+
+            Console.WriteLine($"Uploading {dbFileName}");
+
+            fileService.UploadFile(@"C:\Users\martin.davies\Downloads\pgadmin4-5.5-x64.exe", dbFileName);
+
+            Console.WriteLine($"Downloading {dbFileName}");
+            fileService.DownloadFile(dbFileName, $"C:\\temp\\pgadmin4 - 5.5 - x64.exe");
+
+            Console.WriteLine("Press <any> key to quit.");
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ _projects/GridFS Demo/DB.Core/FileService.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace DB.Core
+{
+    public class FileService
+    {
+        private MongoClient mongoClient = null;
+
+        private IMongoDatabase mongoDatabase = null;
+
+        public FileService(string connectionString)
+        {
+            mongoClient = new (connectionString);
+            mongoDatabase = mongoClient.GetDatabase("fileServer");
+        }
+
+        public void UploadFile(string filePath, string fileName)
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            UploadStream(fileStream, fileName);
+        }
+
+        public ObjectId UploadStream(Stream stream, string fileName)
+        {
+            GridFSBucket bucket = new GridFSBucket(mongoDatabase, new GridFSBucketOptions
+            {
+                BucketName = "files"
+            });
+
+            return bucket.UploadFromStream(fileName, stream);
+        }
+
+        public void DownloadFile(string filename, string outputFileName)
+        {
+            GridFSBucket bucket = new GridFSBucket(mongoDatabase, new GridFSBucketOptions
+            {
+                BucketName = "files"
+            });
+
+            using (Stream stream = bucket.OpenDownloadStreamByName(filename))
+            {
+                using (FileStream outputFileStream = new FileStream(outputFileName, FileMode.Create))
+                {
+                    stream.CopyTo(outputFileStream);
+                }
+            }
+        }
+    }
+}
